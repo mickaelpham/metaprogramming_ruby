cc43cf117cde97bf4275549461d7136274e5c153
Mickael Pham <mickael.pham@gmail.com>
2015-06-12 14:45:56 -0700

    Enter Metaprogramming

--- the_m_word/orm.rb
+++ the_m_word/orm.rb
@@ -1,45 +1,6 @@
-class Entity
-  attr_reader :table, :ident
-
-  def initialize(table, ident)
-    @table = table
-    @ident = ident
-    Database.sql "INSERT INTO #{@table} (id) VALUES (#{@ident})"
-  end
-
-  def set(col, val)
-    Database.sql "UPDATE #{@table} SET #{col} = '#{val}' WHERE id=#{@ident}"
-  end
-
-  def get(col)
-    Database.sql ("SELECT #{col} FROM #{@table} WHERE id=#{@ident}")[0][0]
-  end
+class Movie < ActiveRecord::Base
 end
 
-# Now we can subclass Entity to map to a specific table
-class Movie < Entity
-  def initialize(ident)
-    super "movies", ident
-  end
-
-  def title
-    get "title"
-  end
-
-  def title=(value)
-    set "title", value
-  end
-
-  def director
-    get "director"
-  end
-
-  def director=(value)
-    set "director", value
-  end
-end
-
-# Bob can now load a new movie into the database
-movie          = Movie.new(1)
-movie.title    = "Doctor Strangelove"
-movie.director = "Standley Kubrick"
+movie         = Movie.create
+movie.title   = "Doctor Strangelove"
+movie.title # => "Doctor Strangelove"
