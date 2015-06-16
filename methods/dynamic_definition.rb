1868a8ecd6ccaf1947caad723ec891006dca8db4
Mickael Pham <mickael.pham@gmail.com>
2015-06-16 11:50:17 -0700

    Added documentation for dynamic method

--- methods/dynamic_definition.rb
+++ methods/dynamic_definition.rb
@@ -6,3 +6,8 @@ end
 
 obj = MyClass.new
 obj.my_method(2) # => 6
+
+# This technique of defining a method at runtime is called a _dynamic method_.
+# There is one important reason to use `define_method` over the more familiar
+# `def` keyword: `define_method` allows you to decide the name of the defined
+# method at runtime.
