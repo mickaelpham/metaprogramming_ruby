b0d92aeec5794d43f6f7a16bc60f5830335a13e9
Mickael Pham <mickael.pham@gmail.com>
2015-06-18 14:24:29 -0700

    Better monkeypatching

--- 03-methods/const_missing.rb
+++ 03-methods/const_missing.rb
@@ -6,16 +6,17 @@ module M
 end
 
 # Override const_missing from Module
+# Source http://stackoverflow.com/questions/4470108/when-monkey-patching-a-method-can-you-call-the-overridden-method-from-the-new-i
 class Module
-  alias_method :old_const_missing, :const_missing
+  old_const_missing = instance_method(:const_missing)
 
-  def const_missing(const_name)
+  define_method(:const_missing) do |const_name|
     case const_name
     when :MyClass
       puts "Warning: using deprecated constant #{const_name}"
       M::MyClass
     else
-      old_const_missing const_name
+      old_const_missing(const_name)
     end
   end
 end
