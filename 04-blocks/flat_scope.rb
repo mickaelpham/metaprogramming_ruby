4b7dffec3b2569d23bb10d3bf220ca275428e0bd
Mickael Pham <mickael.pham@gmail.com>
2015-06-18 18:17:10 -0700

    Modified flat scope to solve problem

--- 04-blocks/flat_scope.rb
+++ 04-blocks/flat_scope.rb
@@ -1,9 +1,13 @@
 my_var = "Success"
 
-class MyClass
-  # We want to print my_var here
+MyClass = Class.new do
+  # Now we can print my_var here
+  puts "#{my_var} in the class definition!"
 
-  def my_method
-    # ..and here
+  define_method :my_method do
+    puts "#{my_var} in the method definition!"
   end
 end
+
+puts "About to instantiate my class"
+MyClass.new.my_method
