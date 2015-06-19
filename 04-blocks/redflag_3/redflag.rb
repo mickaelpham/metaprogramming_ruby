e38c44353257ed3c23bc1a497593a770ab865794
Mickael Pham <mickael.pham@gmail.com>
2015-06-19 14:20:34 -0700

    Removing the "global" variables

--- 04-blocks/redflag_3/redflag.rb
+++ 04-blocks/redflag_3/redflag.rb
@@ -1,20 +1,30 @@
 # The DSL should check events, and also execute all the `setup` blocks before
 # each event is evaluated.
 
-def setup(&block)
-  @setups << block
-end
+lambda {
+  setups = []
+  events = []
 
-def event(description, &block)
-  @events << { :description => description, :condition => block }
-end
+  Kernel.send :define_method, :setup do |&block|
+    setups << block
+  end
 
-@setups = []
-@events = []
+  Kernel.send :define_method, :event do |description, &block|
+    events << { :description => description, :condition => block }
+  end
+
+  Kernel.send :define_method, :each_setup do |&block|
+    setups.each { |setup| block.call setup }
+  end
+
+  Kernel.send :define_method, :each_event do |&block|
+    events.each { |event| block.call event }
+  end
+}.call # call the lambda immediately
 
 load 'events.rb'
 
-@events.each do |event|
-  @setups.each { |s| s.call }
+each_event do |event|
+  each_setup { |s| s.call }
   puts "ALERT: #{event[:description]}" if event[:condition].call
 end
