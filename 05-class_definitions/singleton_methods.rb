031f43183585fab9bab73fc86d9d46d23f5e3a92
Mickael Pham <mickael.pham@gmail.com>
2015-06-22 14:48:54 -0700

    Singleton methods added

---
+++ 05-class_definitions/singleton_methods.rb
@@ -0,0 +1,9 @@
+str = "just a regular string"
+
+def str.title?
+  self.upcase == self
+end
+
+str.title?                  # => false
+str.methods.grep(/title?/)  # => [:title?]
+str.singleton_methods       # => [:title?]
